87f22d9d19a4cebf53a9bb8b4a7cd002edffa57d
snore <snoretom@naver.com>
2023-06-29 17:39:39 +0900

    231231231

--- reelreview/src/App.js
+++ reelreview/src/App.js
@@ -1,10 +1,15 @@
 
 import './App.css';
+import { Route,Routes } from "react-router-dom";
+import Detail from './component/details/Detail';
 
 function App() {
   return (
     <div className="App">
-      
+      <Detail></Detail>
+      <Routes>
+        <Route path='/details' Component={<Detail></Detail>}/>
+      </Routes>
     </div>
   );
 }
