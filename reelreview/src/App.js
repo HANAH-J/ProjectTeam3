5682b2b283308aa8140b5b29702b6de250e66320
keepgoing1119 <kiminhyeok1119@gmail.com>
2023-07-03 18:08:25 +0900

    0703final

--- reelreview/src/App.js
+++ reelreview/src/App.js
@@ -6,7 +6,13 @@ import UserProfile from './pages/profile/UserProfile/UserProfile';
 import UserScoreCollection from './pages/profile/UserScoreCollection/UserScoreCollection';
 import MovieToWatch from './pages/profile/MovieToWatch/MovieToWatch';
 import CsMain from "./pages/cs/CsMain";
+import CsFaq from "./pages/cs/csFaq/CsFaq";
+import CsQna from "./pages/cs/csQna/CsQna";
+import CsBoard from "./pages/cs/csBoard/CsBoard";
+import MainPage_loginSuccess from "./pages/main/Mainpage_loginSuccess"
 import MovieCollection from './pages/profile/MovieCollection/MovieCollection';
+
+
 function App() {
   return (
     <div className="App">
@@ -15,14 +21,20 @@ function App() {
         <Route path="/" Component={MainPage}/>
         <Route path="/mainPage" Component={MainPage}/>
         <Route path="/csMain" Component={CsMain}/>
+        <Route path="/csFaq" Component={CsFaq}/>
+        <Route path="/csQna" Component={CsQna}/>
         <Route path='/details/:movieCd' Component={Details}/>
         <Route path="/userProfile" Component={UserProfile} />
+        <Route path="/mainPage_loginSuccess" element={<MainPage_loginSuccess/>} />
         <Route path="/userScoreCollection" element={<UserScoreCollection/>} />
         <Route path="/movieToWatch" element={<MovieToWatch/>} />
+        <Route path="/csBoard" element={<CsBoard/>} />
         <Route path="/movieCollection" element={<MovieCollection/>} />
       </Routes>
+
     </div>
   );
 }
 
 export default App;
+
